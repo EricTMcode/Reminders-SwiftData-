0a59c38f2a19a32a7f5f91e78c432a51cbc94358
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-02-20 13:27:13 +0100

    Initial Commit

---
+++ Reminders(SwiftData)/Reminders_SwiftData_App.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_SwiftData_App.swift
+//  Reminders(SwiftData)
+//
+//  Created by Eric on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_SwiftData_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
